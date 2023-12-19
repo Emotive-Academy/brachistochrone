f243510d290b45c14e0cd6b7e217cceea6a4b46a
Siddhartha Banerjee <sidban@uwalumni.com>
2023-12-18 21:10:00 -0800

    Package directory created
    1. Package directory created
    2. `.gitignore` updated.
    3. Example Hello World! program created.

---
+++ rolypoly/__init__.py
@@ -0,0 +1,24 @@
+# Version number
+__version__ = '0.0.1'
+
+# Author
+__author__ = 'Siddhartha Banerjee'
+
+# License
+__license__ = 'GNU GPLv3'
+
+# Description
+__description__ = 'A Python package to calculate slanted plate rolling of balls in 2D plane'
+
+# URL
+__url__ = 'www.emotive.academy'
+
+# Email
+__email__ = 'info@emotive.academy'
+
+# Maintainer
+__maintainer__ = 'Siddhartha Banerjee'
+
+
+# Imports
+import numpy
